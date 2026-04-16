f859bcce95f8ae0ec9813a05a8b2fd087f1c61ff
Thomas Felton <tdfelton@astral.sciclone.wm.edu>
2026-04-16 10:08:39 -0400

    made 01_train a regular sh script and made the hpc submission inside the pipeline

--- pipeline.sh
+++ pipeline.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
-set -ueo pipefail
+#SBATCH --job-name=fruit_cnn
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=16
+#SBATCH --time=12:00:00          # 12 hours (3 models sequentially)
+#SBATCH --mem=64G
+#SBATCH --mail-type=FAIL,BEGIN,END
+#SBATCH --mail-user=mrmellors@wm.edu
+#SBATCH -o /sciclone/scr10/gzdata440/fruitsdata/logs/fruit_cnn_%j.out
+#SBATCH -e /sciclone/scr10/gzdata440/fruitsdata/logs/fruit_cnn_%j.err
+#SBATCH --gpus=1
 
 ./scripts/00_download_data.sh
 
