f6b5b3a0c6ed95ac4f57398fec09bebe73316e61
Thomas Felton <tdfelton@astral.sciclone.wm.edu>
2026-04-21 09:51:05 -0400

    added additional documentation

--- pipeline.sh
+++ pipeline.sh
@@ -11,8 +11,11 @@
 #SBATCH -e ./logs/fruit_cnn_%j.err
 #SBATCH --gpus=1
 
+##Download the data from kaggle user aelchimminut and create a conda environment with these packages: pytorch torchvision torchaudio numpy pillow kaggle, see .yml file for more specific information
 ./scripts/00_download_data.sh
 
+##Train 3 separate CNN models on the downloaded fruits data, alexnet, alexnet_bn, resnet50. Also calculates each models accuracy in terms of classifying groups and compares the models with different metrics
+##See 01_train_cnn.py and readme for additional explanation
 ./scripts/01_train_cnn.slurm
 
 
